3d1841ca108ec658a3d902cab90a3d9ee9cc8cca
alejandra diaz torres <kellyalejandradiaztorres07@gmail.com>
2026-02-10 09:35:11 -0500

    añadimos archivo config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$servidor="localhost";
+$user="root";
+$password="";
+$basededatos="travelnow1";
+$con=mysqli_connect($servidor,$user,$password,$basededatos)
+or die ("problemas de conexion");
